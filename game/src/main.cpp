65d622e07cb2d934b95d88d95ac7edc3c7839259
Orinn41 <boranakkoyun41@gmail.com>
2025-09-21 13:27:16 -0400

    cached up with the last lab
    cached up with the last lab, will be finishing lab 1 next

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -12,19 +12,35 @@ See documentation here: https://www.raylib.com/, and examples here: https://www.
 const unsigned int TARGET_FPS = 50; //frames/second
 float dt = 1.0f / TARGET_FPS; //seconds/frame
 float time = 0;
-float x = 500;
-float y = 500;
-float frequency = 1;
-float amplitude = 100;
-
+//float x = 500;
+//float y = 500;
+//float frequency = 2;
+//float frequency2 = 1;
+//float amplitude = 100;
+float speed = 100;
+float angle = 0;
+Vector2 position = { 500, 500 };
+Vector2 velocity = { 0,0 };
+Vector2 accelerationGravity = { 0, 9 };
 //Changes world state
 void update()
 {
 	dt = 1.0f / TARGET_FPS;
 	time += dt;
 
-	x = x + (-sin(time * frequency)) * frequency * amplitude * dt;
-	y = y + (cos(time * frequency)) * frequency * amplitude * dt;
+	//vel = change in position / time, therefore Change in position = vel * time
+	position = position + velocity * dt;
+
+	//accel = deltav / time (change in velocity over time) therefore deltaV = accel * time
+	velocity = velocity + accelerationGravity * dt;
+	if (IsKeyPressed ( KEY_SPACE))
+	{
+		position = { 100, (float)GetScreenHeight() - 100 };
+		velocity = { speed * (float)cos(angle * DEG2RAD), -speed * (float)sin(angle * DEG2RAD) };
+	}
+
+	//x = x + (-sin(time * frequency)) * frequency * amplitude * dt;
+	//y = y + (cos(time * frequency)) * frequency * amplitude * dt;
 }
 
 //Display world state
@@ -34,12 +50,26 @@ void draw()
 	ClearBackground(BLACK);
 	DrawText("Game2005_Boran_Akkoyun_101557415", 10, float(GetScreenHeight() - 30), 20, LIGHTGRAY);
 
-
 	GuiSliderBar(Rectangle{ 10, 15, 1000, 20 }, "", TextFormat("%.2f", time), &time, 0, 240);
+
+	GuiSliderBar(Rectangle{ 10, 40, 500, 30 }, "Speed", TextFormat("Speed: %.0f", speed), &speed, -1000, 1000);
+
+	GuiSliderBar(Rectangle{ 10, 80, 500, 30 }, "Angle", TextFormat("Angle: %.Of Degrees", angle), &angle, -180, 180);
+
+	GuiSliderBar(Rectangle{ 10, 120, 500, 30 }, "Gravity Y", TextFormat("Gravity Y: %.0f Px/sec^2", accelerationGravity.y), &accelerationGravity.y, -1000, 1000);
+
 	DrawText(TextFormat("T: %6.2f", time), GetScreenWidth() - 140, 10, 30, LIGHTGRAY);
 
-	DrawCircle(x, y, 70, RED);
-	DrawCircle(500 + cos(time * frequency) * amplitude, 500 + sin(time * frequency) * amplitude, 70, GREEN);
+	Vector2 startPos = { 100, GetScreenHeight() - 100 };
+	Vector2 velocity = { speed * cos(angle * DEG2RAD), -speed * sin(angle * DEG2RAD) };
+
+	DrawLineEx(startPos, startPos + velocity, 3, RED);
+
+	DrawCircle(position.x, position.y, 15, RED);
+
+
+	//DrawCircle(x, y, 70, RED);
+	//DrawCircle(500 + cos(time * frequency2) * amplitude, 500 + sin(time * frequency2) * amplitude, 70, GREEN);
 
 	EndDrawing();
 
